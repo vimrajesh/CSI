e2f1de9da6dcf281a214f9ef88c1b9eed8413fab
AslamAlt <36098039+AslamAlt@users.noreply.github.com>
2021-03-20 13:00:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,6 @@
 var server = require('wss').Server;
 var s = new server({port: 8000});
 var cli_count = 0;
-const TIMEOUT = 5 //seconds
-
-function noop() {}
-
-function heartbeat() {
-  this.isAlive = true;
-  // console.log('pong kittito')
-}
 
 s.on('connection', function(ws, req) {
 	cli_count += 1;
@@ -16,26 +8,13 @@ s.on('connection', function(ws, req) {
 
 	console.log("Client " + cli_count + " connected. IP address: "+ip);
 
-	ws.isAlive = true;
-	ws.on('pong', heartbeat);
-	// ws.on('open', function(ws) {
+	// ws.on('message', function(ws) {
 		
 	// });
-	// ws.send('ping', function(ws) {
-	// 	console.log("Sent ping");
-	// })
+	
 	ws.on('close', function() {
 		console.log("Client " + cli_count + " disconnected");
 		cli_count -= 1;
-		clearInterval(interval);
 	});
 });
 
-const interval = setInterval(function ping() {
-  s.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
-
-    ws.isAlive = false;
-    ws.ping(noop);
-  });
-}, TIMEOUT*1000);
